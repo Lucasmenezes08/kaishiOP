f7f0432c031ad003c368513d2d82d6ed980b31e2
Lucasmenezes08 <lms4@cesar.school>
2025-08-17 13:11:48 -0300

    :construction: Add homepage route and create component.

--- kaishi/src/components/App.tsx
+++ kaishi/src/components/App.tsx
@@ -1,10 +1,18 @@
+import { BrowserRouter , Routes , Route } from "react-router"
+import '../index.css'
+import Homepage from "../routes/Homepage"
+ "../routes/Homepage"
+
+
 export default function App() {
  
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ kaishi/src/routes/Homepage.tsx
@@ -0,0 +1,7 @@
+export default function Homepage (){
+    return (
+        <section className="h-screen w-full bg-slate-950">
+            <p className="text-white">Olá mundo</p>
+        </section>
+    )
+}
