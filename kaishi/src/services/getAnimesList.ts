8e596eed76adb690794ee4969d99109e9aa82343
Lucasmenezes08 <lms4@cesar.school>
2025-08-17 17:33:13 -0300

    :recycle: refactor: Change api get params.

--- kaishi/src/services/getAnimesList.ts
+++ kaishi/src/services/getAnimesList.ts
@@ -1,7 +1,7 @@
 import type { ApiResponse } from "../types/type-services/animeThemeTypes";
 
 
-const API_URL = 'https://api.animethemes.moe/anime?filter[season]=winter,summer,spring&filter[year]=2025,2024,2023&page[size]=40&sort=-year&include=images,animethemes.animethemeentries.videos';
+const API_URL ='https://api.animethemes.moe/anime?filter[season]=winter,summer,spring&filter[year]=2025,2024,2023,2022,2021,2020,2019,2018,2017,2016,2015,2014,2013,2012,2011,2010&[type]=OP&page[size]=80&sort=random&media_format=[TV]&include=images,animethemes.animethemeentries.videos';
 
 export default async function getAnimeList ():Promise<ApiResponse>{
     try {
